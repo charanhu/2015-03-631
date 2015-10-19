a4123adb475b6400bd648adab4b7f89537566e4a
Eddie Gurnee <pegurnee@gmail.com>
2015-10-18 22:29:10 -0400

    suits in correct order

--- assignments/a02/scripts/card_games.js
+++ assignments/a02/scripts/card_games.js
@@ -1,6 +1,6 @@
 const
   SUITS = [
-    "hearts", "spades", "diamonds", "clubs"
+    "spades", "hearts", "diamonds", "clubs"
   ],
   VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
   NUM_PER_SUIT = 13;
